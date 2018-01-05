011d87ac1f1720bbdad870e6fde65cc60342065e
Kyle Bebak <kylebebak@gmail.com>
2018-01-05 15:09:28 -0600

    LocalityListItem and OrganizationListItem extend PureComponent so that re-rendering lists if prop changes in one item isn't expensive

--- src/components/OrganizationListItem/OrganizationListItem.js
+++ src/components/OrganizationListItem/OrganizationListItem.js
@@ -5,56 +5,59 @@ import { fmtBudget } from 'tools/string'
 import Styles from './OrganizationListItem.css'
 
 
-const OrganizationListItem = ({ organization, onClick, focused, onMouseEnter, onMouseLeave }) => {
-  const { name, actions, desc } = organization
-  let budget = 0
-  const countByTags = {}
-  for (const action of actions) {
-    if (!action.action_type) continue
-    if (!countByTags[action.action_type]) {
-      countByTags[action.action_type] = 1
-    } else {
-      countByTags[action.action_type] += 1
+class OrganizationListItem extends React.PureComponent {
+  render() {
+    const { organization, onClick, focused, onMouseEnter, onMouseLeave } = this.props
+    const { name, actions, desc } = organization
+    let budget = 0
+    const countByTags = {}
+    for (const action of actions) {
+      if (!action.action_type) continue
+      if (!countByTags[action.action_type]) {
+        countByTags[action.action_type] = 1
+      } else {
+        countByTags[action.action_type] += 1
+      }
+      if (action.budget) budget += action.budget
     }
-    if (action.budget) budget += action.budget
+    const count = Object.keys(countByTags).map((k) => {
+      return { tag: k, count: countByTags[k] }
+    }).sort((a, b) => a.count - b.count)
+
+    const handleClick = () => { onClick(organization) }
+    const handleMouseEnter = () => { onMouseEnter(organization) }
+    const handleMouseLeave = () => { onMouseLeave(organization) }
+
+    let className = Styles.orgCard
+    if (focused) className = `${Styles.orgCard} ${Styles.orgCardFocused}`
+
+    return (
+      <div
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={className}
+      >
+        <div className={Styles.descriptionContainer}>
+          <span className={Styles.name}>{name}</span>
+          <div className={Styles.tagContainer}>
+            {count.slice(0, 3).map((t, i) => <span key={i} className={Styles.tag}>{t.tag}</span>)}
+          </div>
+          <span className={Styles.desc}>{desc}</span>
+        </div>
+        <div className={Styles.metricsContainer}>
+          <div className={budget > 0 ? Styles.metric : Styles.emptyMetric}>
+            <span className={Styles.label}>Inversión<br />estimada</span>
+            <span className={Styles.value}>{fmtBudget(budget)}</span>
+          </div>
+          <div className={Styles.metric}>
+            <span className={Styles.label}>Proyectos<br />registrados</span>
+            <span className={Styles.value}>{actions.length}</span>
+          </div>
+        </div>
+      </div>
+    )
   }
-  const count = Object.keys(countByTags).map((k) => {
-    return { tag: k, count: countByTags[k] }
-  }).sort((a, b) => a.count - b.count)
-
-  const handleClick = () => { onClick(organization) }
-  const handleMouseEnter = () => { onMouseEnter(organization) }
-  const handleMouseLeave = () => { onMouseLeave(organization) }
-
-  let className = Styles.orgCard
-  if (focused) className = `${Styles.orgCard} ${Styles.orgCardFocused}`
-
-  return (
-    <div
-      onClick={handleClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={className}
-    >
-      <div className={Styles.descriptionContainer}>
-        <span className={Styles.name}>{name}</span>
-        <div className={Styles.tagContainer}>
-          {count.slice(0, 3).map((t, i) => <span key={i} className={Styles.tag}>{t.tag}</span>)}
-        </div>
-        <span className={Styles.desc}>{desc}</span>
-      </div>
-      <div className={Styles.metricsContainer}>
-        <div className={budget > 0 ? Styles.metric : Styles.emptyMetric}>
-          <span className={Styles.label}>Inversión<br />estimada</span>
-          <span className={Styles.value}>{fmtBudget(budget)}</span>
-        </div>
-        <div className={Styles.metric}>
-          <span className={Styles.label}>Proyectos<br />registrados</span>
-          <span className={Styles.value}>{actions.length}</span>
-        </div>
-      </div>
-    </div>
-  )
 }
 
 OrganizationListItem.propTypes = {
