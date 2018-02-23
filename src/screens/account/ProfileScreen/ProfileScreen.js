847f37eba054b612215f6c2d4d24853a78cd09bc
Kyle Bebak <kylebebak@gmail.com>
2018-02-23 15:07:12 -0600

    Adds redux form for ResetPasswordForm in ProfileScreen

--- src/screens/account/ProfileScreen/ProfileScreen.js
+++ src/screens/account/ProfileScreen/ProfileScreen.js
@@ -1,25 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import RaisedButton from 'material-ui/RaisedButton'
-import TextField from 'material-ui/TextField'
+import { reset } from 'redux-form'
+import { connect } from 'react-redux'
 
+import * as Actions from 'src/actions'
 import service, { getBackoff } from 'api/service'
 import Styles from 'screens/account/Form.css'
 import UserForm from './UserForm'
+import ResetPasswordForm from './ResetPasswordForm'
 
 
 class Profile extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      oldPassword: '',
-      password: '',
-      confirmPassword: '',
-      error: false,
-    }
-  }
-
   componentDidMount() {
     this.load()
   }
@@ -28,81 +20,36 @@ class Profile extends React.Component {
     getBackoff(service.getMe, { key: 'me' })
   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
   handleSubmitName = async (values) => {
     const { data } = await service.updateMe(values)
-    if (data) {
-      this.load()
-      this.setState({ error: false })
-    } else {
-      this.setState({ error: true })
-    }
+    if (data) this.load()
   }
 
-  handleSubmitPassword = async () => {
-    const { oldPassword, password, confirmPassword } = this.state
-    if (!oldPassword || password.length < 8 || password !== confirmPassword) {
-      this.setState({ error: true })
-      return
-    }
-
-    this.setState({ disabled: true })
+  handleSubmitPassword = async ({ oldPassword, password }) => {
     const { data } = await service.setPassword(oldPassword, password)
-    if (data) {
-      this.setState({ disabled: false, error: false })
-    } else {
-      this.setState({ disabled: false, error: true })
-    }
+    if (data) this.props.reset()
   }
 
   render() {
-    const { disabled, oldPassword, password, confirmPassword } = this.state
     return (
       <div className={Styles.formContainer}>
         <UserForm onSubmit={this.handleSubmitName} enableReinitialize />
-
-        <div>
-          <TextField
-            type="password"
-            name="oldPassword"
-            value={oldPassword}
-            hintText="Contraseña actual"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <TextField
-            type="password"
-            name="password"
-            value={password}
-            hintText="Contraseña nueva"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <TextField
-            type="password"
-            name="confirmPassword"
-            value={confirmPassword}
-            hintText="Confirmar contraseña nueva"
-            onChange={this.handleChange}
-          />
-        </div>
-        <RaisedButton
-          className={Styles.button}
-          disabled={disabled}
-          label="CAMBIAR CONTRASEÑA"
-          onClick={this.handleSubmitPassword}
-        />
+        <ResetPasswordForm onSubmit={this.handleSubmitPassword} />
       </div>
     )
   }
 }
 
 Profile.propTypes = {
+  onResponse: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
 }
 
-export default Profile
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onResponse: (message, status) => Actions.snackbar(dispatch, { message, status }),
+    reset: () => dispatch(reset('resetPassword')),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Profile)
---
+++ src/screens/account/ProfileScreen/ResetPasswordForm.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import { reduxForm, propTypes as rxfPropTypes } from 'redux-form'
+import RaisedButton from 'material-ui/RaisedButton'
+
+import { TextField } from 'components/Fields'
+import Styles from 'screens/account/Form.css'
+
+
+const ResetPasswordForm = ({ handleSubmit, submitting }) => {
+  return (
+    <React.Fragment>
+      <div>
+        <TextField
+          type="password"
+          name="oldPassword"
+          hintText="Contraseña actual"
+        />
+      </div>
+      <div>
+        <TextField
+          type="password"
+          name="password"
+          hintText="Contraseña nueva"
+        />
+      </div>
+      <div>
+        <TextField
+          type="password"
+          name="confirmPassword"
+          hintText="Confirmar contraseña nueva"
+        />
+      </div>
+      <RaisedButton
+        className={Styles.button}
+        disabled={submitting}
+        label="CAMBIAR CONTRASEÑA"
+        onClick={handleSubmit}
+      />
+    </React.Fragment>
+  )
+}
+
+ResetPasswordForm.propTypes = {
+  ...rxfPropTypes,
+}
+
+const validate = ({ oldPassword, password, confirmPassword }) => {
+  const errors = {}
+  if (!oldPassword) errors.confirmPassword = 'Debes meter tu contraseña actual'
+  if (password !== undefined && password.length < 8) errors.password = 'Debe tener al menos 8 caracteres'
+  if (password !== confirmPassword) errors.confirmPassword = 'Las contraseñas tienen que ser iguales'
+  return errors
+}
+
+export default reduxForm({ form: 'resetPassword', validate })(ResetPasswordForm)
