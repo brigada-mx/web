0c91610076fcc2c92eafb8209a5c7941f92da1c3
Kyle Bebak <kylebebak@gmail.com>
2018-03-07 07:53:33 -0600

    FilterHeader unsets state/muni as necessary

--- src/components/FilterHeader/FilterHeader.js
+++ src/components/FilterHeader/FilterHeader.js
@@ -10,310 +10,319 @@ import { projectTypeByValue } from 'src/choices'
 import Styles from './FilterHeader.css'
 
 
-const FilterHeader = (props) => {
-  const {
-    onStateChange,
-    onMuniChange,
-    onMargChange,
-    onNumActionsChange,
-    onSectorChange,
-    onActionTypeChange,
-    localities,
-    actions,
-    valState,
-    valMuni,
-    valMarg,
-    valNumActions,
-    valSector,
-    valActionType,
-    style = {},
-  } = props
-  if (valMuni.length > 0 && valState.length > 0) onStateChange([])
-
-  const shortenState = (name) => {
-    if (name.toLowerCase() === 'veracruz de ignacio de la llave') return 'Veracruz'
-    return name
+class FilterHeader extends React.Component {
+  componentWillUpdate(nextProps) {
+    const { valState, valMuni, onStateChange, onMuniChange } = this.props
+    if (valState.length === 0 && nextProps.valState.length > 0
+      && valMuni.length > 0) onMuniChange([])
+    if (valMuni.length === 0 && nextProps.valMuni.length > 0
+      && valState.length > 0) onStateChange([])
   }
 
-  const stateOptions = () => {
-    const items = _.uniqBy(localities, l => l.cvegeo_state)
+  render() {
+    const {
+      onStateChange,
+      onMuniChange,
+      onMargChange,
+      onNumActionsChange,
+      onSectorChange,
+      onActionTypeChange,
+      localities,
+      actions,
+      valState,
+      valMuni,
+      valMarg,
+      valNumActions,
+      valSector,
+      valActionType,
+      style = {},
+    } = this.props
 
-    return items.sort((a, b) => {
-      if (a.state_name < b.state_name) return -1
-      if (a.state_name > b.state_name) return 1
-      return 0
-    }).map((i) => {
-      return { value: i.cvegeo_state, label: shortenState(i.state_name) }
-    })
-  }
+    const shortenState = (name) => {
+      if (name.toLowerCase() === 'veracruz de ignacio de la llave') return 'Veracruz'
+      return name
+    }
 
-  const muniOptions = () => {
-    const items = _.uniqBy(localities, l => l.cvegeo_municipality)
+    const stateOptions = () => {
+      const items = _.uniqBy(localities, l => l.cvegeo_state)
 
-    return items.sort((a, b) => {
-      if (a.municipality_name < b.municipality_name) return -1
-      if (a.municipality_name > b.municipality_name) return 1
-      return 0
-    }).map((i) => {
-      return { value: i.cvegeo_municipality, label: `${i.municipality_name}, ${i.state_name}` }
-    })
-  }
+      return items.sort((a, b) => {
+        if (a.state_name < b.state_name) return -1
+        if (a.state_name > b.state_name) return 1
+        return 0
+      }).map((i) => {
+        return { value: i.cvegeo_state, label: shortenState(i.state_name) }
+      })
+    }
 
-  const actionTypeOptions = () => {
-    const items = _.uniqBy(actions, a => a.action_type)
+    const muniOptions = () => {
+      const items = _.uniqBy(localities, l => l.cvegeo_municipality)
 
-    return items.sort((a, b) => {
-      if (a.action_type < b.action_type) return -1
-      if (a.action_type > b.action_type) return 1
-      return 0
-    }).map((i) => {
-      return { value: i.action_type, label: projectTypeByValue[i.action_type] || '?' }
-    })
-  }
+      return items.sort((a, b) => {
+        if (a.municipality_name < b.municipality_name) return -1
+        if (a.municipality_name > b.municipality_name) return 1
+        return 0
+      }).map((i) => {
+        return { value: i.cvegeo_municipality, label: `${i.municipality_name}, ${i.state_name}` }
+      })
+    }
 
-  const selectsLarge = (className) => {
+    const actionTypeOptions = () => {
+      const items = _.uniqBy(actions, a => a.action_type)
+
+      return items.sort((a, b) => {
+        if (a.action_type < b.action_type) return -1
+        if (a.action_type > b.action_type) return 1
+        return 0
+      }).map((i) => {
+        return { value: i.action_type, label: projectTypeByValue[i.action_type] || '?' }
+      })
+    }
+
+    const selectsLarge = (className) => {
+      return (
+        <React.Fragment>
+          <MultiSelect
+            multi
+            joinValues
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valState}
+            placeholder="Estado"
+            multiLabel="Estado"
+            onChange={onStateChange}
+            options={stateOptions()}
+          />
+
+          <MultiSelect
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valMuni}
+            placeholder="Municipio"
+            multiLabel="Municipio"
+            onChange={onMuniChange}
+            options={muniOptions()}
+          />
+
+          {valMarg && <MultiSelect
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valMarg}
+            placeholder="Marginación social"
+            multiLabel="Marginación"
+            onChange={onMargChange}
+            options={[
+              { value: 'muy_alto', label: 'Muy alta' },
+              { value: 'alto', label: 'Alta' },
+              { value: 'medio', label: 'Media' },
+              { value: 'bajo', label: 'Baja' },
+            ]}
+          />}
+
+          {valNumActions && <MultiSelect
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valNumActions}
+            placeholder="Total de proyectos"
+            multiLabel="Proyectos"
+            onChange={onNumActionsChange}
+            options={[
+              { value: '0', label: '0' },
+              { value: '1', label: '1-9' },
+              { value: '2', label: '10-49' },
+              { value: '3', label: '50+' },
+            ]}
+          />}
+
+          {valSector && <MultiSelect
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valSector}
+            placeholder="Sector"
+            multiLabel="Sector"
+            onChange={onSectorChange}
+            options={[
+              { value: 'civil', label: 'Civil' },
+              { value: 'public', label: 'Público' },
+              { value: 'private', label: 'Privado' },
+              { value: 'religious', label: 'Religioso' },
+            ]}
+          />}
+
+          {(valActionType && actions) && <MultiSelect
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            className={className}
+            value={valActionType}
+            placeholder="Tipo de proyecto"
+            multiLabel="Tipo"
+            onChange={onActionTypeChange}
+            options={actionTypeOptions()}
+          />}
+        </React.Fragment>
+      )
+    }
+
+    const selectsSmall = (className) => {
+      return (
+        <React.Fragment>
+          {valMarg &&
+            <React.Fragment>
+              <span className={Styles.title}>Marginación social</span>
+              <Select
+                multi
+                autoFocus
+                openOnFocus
+                noResultsText="Cero resultados"
+                clearable={false}
+                closeOnSelect={false}
+                removeSelected={false}
+                searchable={false}
+                className={className}
+                value={valMarg}
+                placeholder=""
+                onChange={onMargChange}
+                options={[
+                  { value: 'muy_alto', label: 'Muy alta' },
+                  { value: 'alto', label: 'Alta' },
+                  { value: 'medio', label: 'Media' },
+                  { value: 'bajo', label: 'Baja' },
+                ]}
+              />
+            </React.Fragment>
+          }
+          {valNumActions &&
+            <React.Fragment>
+              <span className={Styles.title}>Total de proyectos</span>
+              <Select
+                multi
+                noResultsText="Cero resultados"
+                clearable={false}
+                closeOnSelect={false}
+                removeSelected={false}
+                searchable={false}
+                className={className}
+                value={valNumActions}
+                placeholder=""
+                onChange={onNumActionsChange}
+                options={[
+                  { value: '0', label: '0' },
+                  { value: '1', label: '1-9' },
+                  { value: '2', label: '10-49' },
+                  { value: '3', label: '50+' },
+                ]}
+              />
+            </React.Fragment>
+          }
+          {valSector &&
+            <React.Fragment>
+              <span className={Styles.title}>Sector</span>
+              <Select
+                multi
+                autoFocus
+                openOnFocus
+                noResultsText="Cero resultados"
+                clearable={false}
+                closeOnSelect={false}
+                removeSelected={false}
+                searchable={false}
+                className={className}
+                value={valSector}
+                placeholder=""
+                onChange={onSectorChange}
+                options={[
+                  { value: 'civil', label: 'Civil' },
+                  { value: 'public', label: 'Público' },
+                  { value: 'private', label: 'Privado' },
+                  { value: 'religious', label: 'Religioso' },
+                ]}
+              />
+            </React.Fragment>
+          }
+          {(valActionType && actions) &&
+            <React.Fragment>
+              <span className={Styles.title}>Tipo de proyecto</span>
+              <Select
+                multi
+                noResultsText="Cero resultados"
+                clearable={false}
+                closeOnSelect={false}
+                removeSelected={false}
+                searchable={false}
+                className={className}
+                value={valActionType}
+                placeholder=""
+                onChange={onActionTypeChange}
+                options={actionTypeOptions()}
+              />
+            </React.Fragment>
+          }
+          <span className={Styles.title}>Estado</span>
+          <Select
+            multi
+            joinValues
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            searchable={false}
+            className={className}
+            value={valState}
+            placeholder=""
+            onChange={onStateChange}
+            options={stateOptions()}
+          />
+          <span className={Styles.title}>Municipio</span>
+          <Select
+            multi
+            noResultsText="Cero resultados"
+            clearable={false}
+            closeOnSelect={false}
+            removeSelected={false}
+            searchable={false}
+            className={className}
+            value={valMuni}
+            placeholder=""
+            onChange={onMuniChange}
+            options={muniOptions()}
+          />
+        </React.Fragment>
+      )
+    }
+
     return (
       <React.Fragment>
-        <MultiSelect
-          multi
-          joinValues
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valState}
-          placeholder="Estado"
-          multiLabel="Estado"
-          onChange={onStateChange}
-          options={stateOptions()}
-        />
-
-        <MultiSelect
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valMuni}
-          placeholder="Municipio"
-          multiLabel="Municipio"
-          onChange={onMuniChange}
-          options={muniOptions()}
-        />
-
-        {valMarg && <MultiSelect
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valMarg}
-          placeholder="Marginación social"
-          multiLabel="Marginación"
-          onChange={onMargChange}
-          options={[
-            { value: 'muy_alto', label: 'Muy alta' },
-            { value: 'alto', label: 'Alta' },
-            { value: 'medio', label: 'Media' },
-            { value: 'bajo', label: 'Baja' },
-          ]}
-        />}
-
-        {valNumActions && <MultiSelect
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valNumActions}
-          placeholder="Total de proyectos"
-          multiLabel="Proyectos"
-          onChange={onNumActionsChange}
-          options={[
-            { value: '0', label: '0' },
-            { value: '1', label: '1-9' },
-            { value: '2', label: '10-49' },
-            { value: '3', label: '50+' },
-          ]}
-        />}
-
-        {valSector && <MultiSelect
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valSector}
-          placeholder="Sector"
-          multiLabel="Sector"
-          onChange={onSectorChange}
-          options={[
-            { value: 'civil', label: 'Civil' },
-            { value: 'public', label: 'Público' },
-            { value: 'private', label: 'Privado' },
-            { value: 'religious', label: 'Religioso' },
-          ]}
-        />}
-
-        {(valActionType && actions) && <MultiSelect
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          className={className}
-          value={valActionType}
-          placeholder="Tipo de proyecto"
-          multiLabel="Tipo"
-          onChange={onActionTypeChange}
-          options={actionTypeOptions()}
-        />}
+        <div style={style} className={`${Styles.container} sm-hidden xs-hidden`}>
+          {selectsLarge(Styles.filter)}
+        </div>
+        <div style={style} className={`${Styles.container} lg-hidden md-hidden wrapper-sm wrapper-xs`}>
+          {selectsSmall(Styles.filter)}
+        </div>
       </React.Fragment>
     )
   }
-
-  const selectsSmall = (className) => {
-    return (
-      <React.Fragment>
-        {valMarg &&
-          <React.Fragment>
-            <span className={Styles.title}>Marginación social</span>
-            <Select
-              multi
-              autoFocus
-              openOnFocus
-              noResultsText="Cero resultados"
-              clearable={false}
-              closeOnSelect={false}
-              removeSelected={false}
-              searchable={false}
-              className={className}
-              value={valMarg}
-              placeholder=""
-              onChange={onMargChange}
-              options={[
-                { value: 'muy_alto', label: 'Muy alta' },
-                { value: 'alto', label: 'Alta' },
-                { value: 'medio', label: 'Media' },
-                { value: 'bajo', label: 'Baja' },
-              ]}
-            />
-          </React.Fragment>
-        }
-        {valNumActions &&
-          <React.Fragment>
-            <span className={Styles.title}>Total de proyectos</span>
-            <Select
-              multi
-              noResultsText="Cero resultados"
-              clearable={false}
-              closeOnSelect={false}
-              removeSelected={false}
-              searchable={false}
-              className={className}
-              value={valNumActions}
-              placeholder=""
-              onChange={onNumActionsChange}
-              options={[
-                { value: '0', label: '0' },
-                { value: '1', label: '1-9' },
-                { value: '2', label: '10-49' },
-                { value: '3', label: '50+' },
-              ]}
-            />
-          </React.Fragment>
-        }
-        {valSector &&
-          <React.Fragment>
-            <span className={Styles.title}>Sector</span>
-            <Select
-              multi
-              autoFocus
-              openOnFocus
-              noResultsText="Cero resultados"
-              clearable={false}
-              closeOnSelect={false}
-              removeSelected={false}
-              searchable={false}
-              className={className}
-              value={valSector}
-              placeholder=""
-              onChange={onSectorChange}
-              options={[
-                { value: 'civil', label: 'Civil' },
-                { value: 'public', label: 'Público' },
-                { value: 'private', label: 'Privado' },
-                { value: 'religious', label: 'Religioso' },
-              ]}
-            />
-          </React.Fragment>
-        }
-        {(valActionType && actions) &&
-          <React.Fragment>
-            <span className={Styles.title}>Tipo de proyecto</span>
-            <Select
-              multi
-              noResultsText="Cero resultados"
-              clearable={false}
-              closeOnSelect={false}
-              removeSelected={false}
-              searchable={false}
-              className={className}
-              value={valActionType}
-              placeholder=""
-              onChange={onActionTypeChange}
-              options={actionTypeOptions()}
-            />
-          </React.Fragment>
-        }
-        <span className={Styles.title}>Estado</span>
-        <Select
-          multi
-          joinValues
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          searchable={false}
-          className={className}
-          value={valState}
-          placeholder=""
-          onChange={onStateChange}
-          options={stateOptions()}
-        />
-        <span className={Styles.title}>Municipio</span>
-        <Select
-          multi
-          noResultsText="Cero resultados"
-          clearable={false}
-          closeOnSelect={false}
-          removeSelected={false}
-          searchable={false}
-          className={className}
-          value={valMuni}
-          placeholder=""
-          onChange={onMuniChange}
-          options={muniOptions()}
-        />
-      </React.Fragment>
-    )
-  }
-
-  return (
-    <React.Fragment>
-      <div style={style} className={`${Styles.container} sm-hidden xs-hidden`}>
-        {selectsLarge(Styles.filter)}
-      </div>
-      <div style={style} className={`${Styles.container} lg-hidden md-hidden wrapper-sm wrapper-xs`}>
-        {selectsSmall(Styles.filter)}
-      </div>
-    </React.Fragment>
-  )
 }
 
 FilterHeader.propTypes = {
