6f09262aec0b3cf531892871ff18ce81cc9eb005
ermancera <er.mancera@gmail.com>
2018-03-26 17:44:38 -0600

    Added styles to account created text and updated content

--- src/screens/account/AccountCreated/AccountCreated.js
+++ src/screens/account/AccountCreated/AccountCreated.js
@@ -8,10 +8,8 @@ import Styles from './AccountCreated.css'
 const AccountCreated = ({ email }) => {
   return (
     <div className={Styles.container}>
-      <span className={FormStyles.formHeader}>¡Registraste tu cuenta!</span>
-      <p className={Styles.info}>Hemos mandado un correo a {email} para que definas tu contraseña y actives tu cuenta. Por favor revisa tu correo.</p>
-      <p className={Styles.info}>Para empezar a documentar tus proyectos de reconstrucción, es necesario tomar una capacitación de 30 minutos via video-chat.</p>
-      <a href="https://calendly.com/brigada/capacitacion" target="_blank" className={Styles.primaryButton}>AGENDAR VIDEO-CHAT</a>
+      <span className={FormStyles.formHeader}>¡Gracias por registrarte!</span>
+      <p className={FormStyles.formText}>Hemos enviado un correo a {email} para que actives tu cuenta.</p>
     </div>
   )
 }
