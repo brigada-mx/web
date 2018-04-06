31bf84842e1abd851476dea308a05c25f8342d62
Kyle Bebak <kylebebak@gmail.com>
2018-04-05 21:33:35 -0500

    Finishes DonorListScreenView, except for styles

--- src/screens/DonorListScreen/DonorListScreenView.js
+++ src/screens/DonorListScreen/DonorListScreenView.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import ReactTable from 'react-table'
+import { withRouter } from 'react-router-dom'
+import '!style-loader!css-loader!react-table/react-table.css'
+
+import { sectorByValue } from 'src/choices'
+import { fmtBudget } from 'tools/string'
 import LoadingIndicatorCircle from 'components/LoadingIndicator/LoadingIndicatorCircle'
 import Styles from './DonorListScreenView.css'
 
@@ -10,18 +16,80 @@ class DonorListScreenView extends React.Component {
     document.title = 'Donadores - Brigada'
   }
 
+  renderAddress = (address = {}) => {
+    const { locality, city } = address
+    const parts = []
+    if (locality) parts.push(locality)
+    if (city) parts.push(city)
+    return parts.join(', ')
+  }
+
   render() {
     const { donors: { data: donorData, loading: donorLoading, error: donorError } } = this.props
+    const { history } = this.props
 
     const donors = donorData ? donorData.results : []
 
     if (donorLoading) return <LoadingIndicatorCircle className={Styles.loader} />
-    return <div>Donadores</div>
+
+    const columns = [
+      {
+        Header: 'Donador',
+        accessor: 'name',
+      },
+      {
+        Header: 'Ubicación',
+        accessor: 'contact.address',
+        Cell: props => this.renderAddress(props.original.contact.address) || 'No disponible',
+      },
+      {
+        Header: 'Sector',
+        accessor: 'sector',
+        Cell: props => sectorByValue[props.original.sector] || 'No disponible',
+      },
+      {
+        Header: 'Reconstructores financiados',
+        accessor: 'metrics.org_count',
+      },
+      {
+        Header: 'Proy. financiados',
+        accessor: 'metrics.action_count',
+      },
+      {
+        Header: 'Monto donado',
+        accessor: 'metrics.total_donated',
+        Cell: props => fmtBudget(props.original.metrics.total_donated),
+      },
+    ]
+
+    return (
+      <div className={Styles.tableWrapper}>
+        <ReactTable
+          showPagination={false}
+          showPageSizeOptions={false}
+          className="-highlight"
+          defaultPageSize={donors.length}
+          data={donors}
+          columns={columns}
+          getTdProps={(state, rowInfo, column) => {
+            const handleRowClicked = (e, handleOriginal) => {
+              history.push(`/donadores/${rowInfo.original.id}`)
+              if (handleOriginal) handleOriginal()
+            }
+            return {
+              onClick: handleRowClicked,
+              style: { cursor: 'pointer' },
+            }
+          }}
+        />
+      </div>
+    )
   }
 }
 
 DonorListScreenView.propTypes = {
   donors: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 }
 
-export default DonorListScreenView
+export default withRouter(DonorListScreenView)
