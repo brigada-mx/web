c10d4fcedcc0d858e7892bd1b1e78a3aef1340c1
Kyle Bebak <kylebebak@gmail.com>
2018-04-07 15:48:46 -0500

    AccountCreated also handles "donor" type

--- src/screens/account/AccountCreated/AccountCreated.js
+++ src/screens/account/AccountCreated/AccountCreated.js
@@ -10,13 +10,29 @@ const AccountCreated = ({ email, type }) => {
     return (
       <div className={Styles.container}>
         <span className={FormStyles.formHeader}>¡Gracias por registrarte!</span>
-        <p className={FormStyles.formText}>Hemos enviado un correo a {email} para que actives tu cuenta.</p>
+        <p className={FormStyles.formText}>
+          Hemos enviado un correo a {email} para que actives tu cuenta.
+        </p>
       </div>
     )
   }
   if (type === 'donor') {
-
+    return (
+      <div className={Styles.container}>
+        <span className={FormStyles.formHeader}>¡Gracias por registrarte!</span>
+        <p className={FormStyles.formText}>
+          Hemos enviado un correo a {email} para que actives tu cuenta.
+        </p>
+        <p className={FormStyles.formText}>
+          Por razones de seguridad, no puedes entrar a tu cuenta hasta que el equipo de Brigada verifique tu identificación. Típicamente, este proceso tarda menos de un día.
+        </p>
+        <p className={FormStyles.formText}>
+          Te mandaremos un email en cuanto terminemos la verificación.
+        </p>
+      </div>
+    )
   }
+  return null
 }
 
 AccountCreated.propTypes = {
