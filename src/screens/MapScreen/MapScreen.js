1f56b4c0aadf4d59ab44ccfae1f60f6bf8e3afbf
ermancera <er.mancera@gmail.com>
2018-04-17 18:28:07 -0500

    Fixed Firefox issues on all screen sizes

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -271,7 +271,7 @@ class MapScreen extends React.Component {
               />
             }
           </div>
-          <div className="col-lg-9 col-md-9 col-sm-8 col-xs-4">
+          <div className={`${Styles.flexOverflowTwo} col-lg-9 col-md-9 col-sm-8 col-xs-4`}>
             <div className={Styles.mapContainer}>
               <MapErrorBoundary>
                 <LocalityDamageMap
--- src/screens/OrganizationListScreen/OrganizationListScreenView.js
+++ src/screens/OrganizationListScreen/OrganizationListScreenView.js
@@ -276,7 +276,7 @@ class OrganizationListScreenView extends React.Component {
               />
             }
           </div>
-          <div className="col-lg-6 col-md-6 col-sm-8 col-xs-4">
+          <div className={`${Styles.flexOverflowTwo} col-lg-6 col-md-6 col-sm-8 col-xs-4`}>
             <div className={Styles.mapContainer}>
               <MapErrorBoundary>
                 <LocalityDamageMap
