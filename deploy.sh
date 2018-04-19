9e31f522e36de07c9f93470200ad9b65ac1989dc
Kyle Bebak <kylebebak@gmail.com>
2018-04-19 12:29:50 -0500

    Logs app version on deploy, organizes .gitignore

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,5 @@ aws s3 cp dist/sitemap.xml s3://${bucketname} --acl public-read
 aws s3 cp dist/google2056132fa8ef12ca.html s3://${bucketname} --acl public-read
 
 aws cloudfront create-invalidation --distribution-id E26AO23800RE7C --paths '/*'
+
+curl -d '{"app_type":"web", "git_hash":"'`git rev-parse HEAD`'"}' -H "Content-Type: application/json" -H "Authorization: Bearer `cat .secret-key`" -X POST https://api.brigada.mx/api/internal/app_versions/
