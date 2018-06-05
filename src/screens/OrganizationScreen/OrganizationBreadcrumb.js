7d6e7e8cde88bd0347f80e6147fecb82b65d05a1
Kyle Bebak <kylebebak@gmail.com>
2018-06-04 19:23:13 -0500

    Hides first two org breadcrumb links on small screens for volunteer opportunities

--- src/screens/OrganizationScreen/OrganizationBreadcrumb.js
+++ src/screens/OrganizationScreen/OrganizationBreadcrumb.js
@@ -8,6 +8,17 @@ import Styles from './OrganizationScreenView.css'
 
 
 const OrganizationBreadcrumb = ({ name, sector, id, projectType, actionId, position }) => {
+  const leadingLinks = () => {
+    if (position && window.innerWidth < 768) return null
+    return (
+      <React.Fragment>
+        <span className={Styles.orgList}><Link to="/reconstructores">Reconstructores</Link></span>
+        <span className={Styles.sector}>
+          <Link to={`/reconstructores?sec=${sector}`}>{sectorByValue[sector] || sector}</Link>
+        </span>
+      </React.Fragment>
+    )
+  }
   const optionalLinks = () => {
     if (!projectType) return null
     if (!position) {
@@ -23,13 +34,12 @@ const OrganizationBreadcrumb = ({ name, sector, id, projectType, actionId, posit
 
   return (
     <div className={Styles.breadcrumbLinks}>
-      <span className={Styles.orgList}><Link to="/reconstructores">Reconstructores</Link></span>
-      <span className={Styles.sector}>
-        <Link to={`/reconstructores?sec=${sector}`}>{sectorByValue[sector] || sector}</Link>
-      </span>
+      {leadingLinks()}
+
       <span className={Styles.orgDetail}>
         <Link to={`/reconstructores/${id}`}>{name}</Link>
       </span>
+
       {optionalLinks()}
     </div>
   )
