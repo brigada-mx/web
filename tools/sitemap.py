b764a45d3ebc5502d73bcac5e66509732cf24997
Kyle Bebak <kylebebak@gmail.com>
2018-06-29 14:49:37 -0500

    sitemap.py now includes volunteer opportunity detail pages

--- tools/sitemap.py
+++ tools/sitemap.py
@@ -16,6 +16,9 @@ SITEMAP_TEMPLATE = """<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instan
 <loc>https://app.brigada.mx/donadores</loc>
 </url>
 <url>
+<loc>https://app.brigada.mx/voluntarios</loc>
+</url>
+<url>
 <loc>https://app.brigada.mx/cuenta</loc>
 </url>
 <url>
@@ -38,6 +41,10 @@ def donor_url(pk):
     return f'https://app.brigada.mx/donadores/{pk}'
 
 
+def opportunity_url(pk):
+    return f'https://app.brigada.mx/voluntariado/{pk}'
+
+
 def generate_sitemap(urls):
     elements = [URL_TEMPLATE.format(url) for url in urls]
     return SITEMAP_TEMPLATE.format(''.join(elements))
@@ -52,5 +59,9 @@ if __name__ == '__main__':
     r.raise_for_status()
     donor_urls = [donor_url(donor['id']) for donor in r.json()['results']]
 
+    r = requests.get('https://api.brigada.mx/api/volunteer_opportunities/', params={'page_size': 1000}, timeout=15)
+    r.raise_for_status()
+    opportunity_urls = [opportunity_url(o['id']) for o in r.json()['results']]
+
     with open('dist/sitemap.xml', 'w') as file:
-        file.write(generate_sitemap(org_urls + donor_urls))
+        file.write(generate_sitemap(org_urls + donor_urls + opportunity_urls))
