a36e9a6b22105173ec812b3ed88dfc59c5446b52
Kyle Bebak <kylebebak@gmail.com>
2018-07-02 17:32:54 -0500

    deploy.sh sleeps until API is up before POSTING git hash

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-# `aws` command depends on credentials in `~/.aws` directory
 python3 ./tools/sitemap.py
 ./build.sh
 
 bucketname='719s-web'
 
+# `aws` command depends on credentials in `~/.aws` directory
 aws s3 cp dist/index.html s3://${bucketname} --acl public-read
 aws s3 cp dist/bundle.js s3://${bucketname} --acl public-read
 aws s3 cp dist/bundle.css s3://${bucketname} --acl public-read
@@ -15,5 +15,11 @@ aws s3 cp dist/sitemap.xml s3://${bucketname} --acl public-read
 aws s3 cp dist/google2056132fa8ef12ca.html s3://${bucketname} --acl public-read
 
 aws cloudfront create-invalidation --distribution-id E26AO23800RE7C --paths '/*'
+
+# sleep until API is up
+until curl -s https://api.brigada.mx/api/ > /dev/null; do
+  >&2 echo "API is unavailable - sleeping"
+  sleep 1
+done
 
 curl -d '{"app_type":"web", "git_hash":"'`git rev-parse HEAD`'"}' -H "Content-Type: application/json" -H "Authorization: Bearer `cat .internal-auth-key`" -X POST https://api.brigada.mx/api/internal/app_versions/
