3f374d4dc83ea1e245e172d0defd2f9af32c4623
Kyle Bebak <kylebebak@gmail.com>
2019-04-26 12:27:24 -0500

    Removes pre-commit hook flow check; changes admin email to contacto@brigada.mx

--- src/screens/account/AccountCreated/AccountVerified.js
+++ src/screens/account/AccountCreated/AccountVerified.js
@@ -16,7 +16,7 @@ const AccountVerified = ({ type }) => {
     <div className={Styles.container}>
       <span className={FormStyles.formHeader}>¡Gracias por verificar tu cuenta!</span>
       <p className={Styles.formText}>{text}</p>
-      <p className={Styles.formText}>Pero todavía falta un paso para convertirte en usuario experto: ¡hacer la capacitación! Manda un email a <ReactGA.OutboundLink eventLabel="scheduleTraining" to="mailto:eduardo@brigada.mx?Subject=Brigada Agendar Capacitación">eduardo@brigada.mx</ReactGA.OutboundLink> con tu disponibilidad.</p>
+      <p className={Styles.formText}>Pero todavía falta un paso para convertirte en usuario experto: ¡hacer la capacitación! Manda un email a <ReactGA.OutboundLink eventLabel="scheduleTraining" to="mailto:contacto@brigada.mx?Subject=Brigada Agendar Capacitación">contacto@brigada.mx</ReactGA.OutboundLink> con tu disponibilidad.</p>
       <p className={Styles.formText}>
         Dale clic en el link para agendar tu capacitación.
       </p>
